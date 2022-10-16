740109dfe329ac09441bd227ff62f54a4ae65869
gabs-v <gabyvazquez8297@gmail.com>
2022-10-15 23:23:06 -0700

    form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -15,13 +15,30 @@ const Form = (props) => {
         .catch((err) => console.log(err))
     }
 
+    const createNewEntry = (e) => {
+        e.preventDefault();
+
+        const newEntry = {
+            description:description,
+            discoveries:discoveries
+        };
+        console.log("New entry created!!")
+    }
+
     return (
         <div className='container'>
             <h1>Code Diary</h1>
             <div>
                 <h3>Create New Entry</h3>
-                <form>
-
+                <form onSubmit={createNewEntry}>
+                    <div>
+                        <label>Description:</label>
+                        <textarea rows='5' cols='50'  onChange={ (e) => setDescription(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>Discoveries:</label>
+                        <textarea rows='5' cols='50' onChange={ (e) => setDiscoveries(e.target.value)} />
+                    </div>
                 </form>
             </div>
         </div>
